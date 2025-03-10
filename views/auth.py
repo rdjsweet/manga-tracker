dd1afec899773985ddce63af4f91dc3d60c664e1
rdjsweet <rdjsweet@gmail.com>
2025-03-10 00:58:54 -0400

    Fixed typo

--- views/auth.py
+++ views/auth.py
@@ -82,7 +82,7 @@ def login():
                 cursor.close()
 
             connection.close()
-            flash('Login successful! Let\'s get read some manga!', 'success')
+            flash('Login successful! Let\'s read some manga!', 'success')
             return redirect(url_for('manga.index'))
         else:
             connection.close()
